1e2027201b191c86af97da243321bccf90ab7827
Harsha Vardhan Relangi <harsharelangi43@gmail.com>
2025-10-16 00:43:08 -0500

    Added ContainsDuplicate solution under Arrays topic

---
+++ Arrays/ConatinsDuplicate.java
@@ -0,0 +1,13 @@
+#Using HashMap / 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int val : map.values()){
+            if(val>1) return true;
+        }
+        return false;
+    }
+}
