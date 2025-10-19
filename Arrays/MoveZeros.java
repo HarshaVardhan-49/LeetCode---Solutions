4797c2334ddaa01764054cc66679b505a305d29c
Harsha Vardhan Relangi <harsharelangi43@gmail.com>
2025-10-18 22:20:26 -0500

    test code

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -1,6 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
+        int k=0;
         for(int j=0;j<nums.length;j++){
              if(nums[j] != 0){
                 if(i != j){
